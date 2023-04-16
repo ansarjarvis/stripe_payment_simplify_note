784ec33fecd5e4377eb15b1866d07d1686708a22
Mohammad Ansar <ansar.ofcl00@gmail.com>
2023-04-17 01:16:13 +0530

    add payment method for india

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import stripeRouter from "./routes/stripe.js";
+import Stripe from "stripe";
+
+/* configuration */
+
+dotenv.config();
+let app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+/* Stripe Configuration */
+export let stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+/* routes */
+
+app.use("/create-checkout-session", stripeRouter);
+
+/* Database */
+let port = process.env.PORT || 8000;
+app.listen(8000, () => {
+  console.log(`server is live at port ${port}`);
+});
---
+++ backend/routes/stripe.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { stripe } from "../app.js";
+
+let Router = express.Router();
+
+let storeItems = new Map([
+  [1, { priceInRupees: 39900, name: "Yearly Subscription" }],
+  //   [2, { priceInRupees: 350, name: "secondItem" }],
+]);
+
+Router.post("/", async (req, res) => {
+  try {
+    let session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: req.body.items.map((item) => {
+        let storeItem = storeItems.get(item.id);
+        return {
+          price_data: {
+            currency: "INR",
+            product_data: {
+              name: storeItem.name,
+            },
+            unit_amount: storeItem.priceInRupees,
+          },
+          quantity: item.quantity,
+        };
+      }),
+      success_url: `${process.env.SERVER_URL}/success.html`,
+      cancel_url: `${process.env.SERVER_URL}/cancel.html`,
+    });
+    res.json({ url: session.url });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+export default Router;
