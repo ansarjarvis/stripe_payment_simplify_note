700995d3b44bd7881626c18a3a1294d306c79fa9
Mohammad Ansar <ansar.ofcl00@gmail.com>
2023-04-17 14:12:39 +0530

    add monthly and yearly subscription

--- backend/public/script.js
+++ backend/public/script.js
@@ -1,6 +1,6 @@
-let button = document.querySelector("button");
-button.addEventListener("click", () => {
-  fetch("/create-checkout-session", {
+let yearlyButton = document.querySelector(".yearly-btn");
+yearlyButton.addEventListener("click", () => {
+  fetch("/create-checkout-session/yearly", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -9,6 +9,32 @@ button.addEventListener("click", () => {
       items: [
         { id: 1, quantity: 1 },
         // { id: 2, quantity: 1 },
+      ],
+    }),
+  })
+    .then((res) => {
+      if (res.ok) return res.json();
+      return res.json().then((json) => Promise.reject(json));
+    })
+    .then(({ url }) => {
+      window.location = url;
+    })
+    .catch((e) => {
+      console.error(e.error);
+    });
+});
+
+let monthlyButton = document.querySelector(".monthly-btn");
+monthlyButton.addEventListener("click", () => {
+  fetch("/create-checkout-session/monthly", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      items: [
+        // { id: 1, quantity: 1 },
+        { id: 2, quantity: 1 },
       ],
     }),
   })
--- backend/routes/stripe.js
+++ backend/routes/stripe.js
@@ -5,10 +5,37 @@ let Router = express.Router();
 
 let storeItems = new Map([
   [1, { priceInRupees: 39900, name: "Yearly Subscription" }],
-  //   [2, { priceInRupees: 350, name: "secondItem" }],
+  [2, { priceInRupees: 4900, name: "Monthly Subscription" }],
 ]);
 
-Router.post("/", async (req, res) => {
+Router.post("/yearly", async (req, res) => {
+  try {
+    let session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: req.body.items.map((item) => {
+        let storeItem = storeItems.get(item.id);
+        return {
+          price_data: {
+            currency: "INR",
+            product_data: {
+              name: storeItem.name,
+            },
+            unit_amount: storeItem.priceInRupees,
+          },
+          quantity: item.quantity,
+        };
+      }),
+      success_url: `${process.env.SERVER_URL}/success.html`,
+      cancel_url: `${process.env.SERVER_URL}/cancel.html`,
+    });
+    res.json({ url: session.url });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+Router.post("/monthly", async (req, res) => {
   try {
     let session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
